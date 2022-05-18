82982383b8a066b276071142ab5af2b31b098488
me-mariosteve <mariosteve069@gmail.com>
2022-05-18 15:38:29 +0200

    finally learnt that X and Y are reversed in constructor for cv::Mat, so video is now readable

--- main.cpp
+++ main.cpp
@@ -1,37 +1,47 @@
 # include "includes/includes.hpp"
 
 
+
 int main(int argc, char **argv) {
 
-    const int X = 480, Y = 360, TOTAL_FRAMES = 10;
-
-// setup the video output
-    cv::VideoWriter videoOutput(
-        "out.avi", // file name
-        cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), // codec
-        5, // fps
-        cv::Size(Y, X), // dimensions; it doesn't work with (X, Y)...
-        1 // colors
-        );
+    const int X = 480, Y = 360, FPS = 1, TOTAL_FRAMES = 3;
+    const std::string OUTPUT_FILE = "out.avi";
+    const int CODEC = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+    const cv::Size SIZE(X, Y);
+    const bool IS_COLOR = true;
+    
+    // setup the video output
+    cv::VideoWriter videoOutput;
+    if (!videoOutput.open(
+            OUTPUT_FILE,
+            CODEC,
+            FPS,
+            SIZE, // dimensions
+            IS_COLOR // colors
+            )) {
+        std::cerr << "Failed to open video." << std::endl;
+    }
     
     // generate points
     // r2(x0, x1, stepx, y0, y1, stepy, Point(x, y, sx, sy, radius, mass, color))
-    std::vector<PointI> pts = Generator::r2(0, X, 60, 0, Y, 60, PointI(0, 0, 0, 0, 10, 1, cv::Vec3b(0, 0, 255)));
+    std::vector<PointI> pts = Generator::r2(0, X, 60, 0, Y, 60, PointI(0, 0, 0, 0, 4, 1, cv::Vec3b(0, 0, 255)));
     
     int time = 0;
     while (time < TOTAL_FRAMES) {
 
         // write the frame to the video
-        cv::Mat frame(X, Y, CV_8UC3, cv::Vec3b(0, 0, 0));
+        cv::Mat frame(Y, X, CV_8UC3, cv::Vec3b(0, 0, 0));
         draw(pts, frame); // something goes wrong here, the points aren't drawn after the first frame
-        videoOutput << frame;
+        videoOutput.write(frame);
 
         // display the frame
-        cv::destroyAllWindows();
-        cv::imshow("Frame " + std::to_string(time), frame);
-        cv::waitKey(0);
-
-        std::cerr << "Frame " << time << ": " << pts << std::endl;
+        /*
+          cv::destroyAllWindows();
+          cv::imshow("Frame " + std::to_string(time), frame);
+          cv::waitKey(0);
+        */
+        
+        std::cerr << "Frame " << time << std::endl;
 
         // update the points
         // apply(std::vector<T>, (T, T) -> T, (std::vector<T>) -> T)
